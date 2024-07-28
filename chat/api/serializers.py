568b6af73aec6bc01e9d4fa9e6354c9e49925b19
Azizbek Sobitov <asobitov2005@gmail.com>
2024-07-28 11:40:03 +0500

    feat: full info

--- chat/api/serializers.py
+++ chat/api/serializers.py
@@ -27,6 +27,7 @@ class RoomSerializer(serializers.ModelSerializer):
 
 class MessageSerializer(serializers.ModelSerializer):
     room = RoomSerializer()
+
     class Meta:
         model = Message
         fields = ['id', 'room', 'sender', 'message']
