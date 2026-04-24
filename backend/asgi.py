b97ad98b153df5aef21ae68381ca860fd8b148e4
asobitov2005 <76997077+asobitov2005@users.noreply.github.com>
2026-04-24 19:28:00 +0500

    fix: initialize ASGI before routing imports

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,14 +1,15 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
 from django.core.asgi import get_asgi_application
+
+http_response_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
-http_response_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
